1e759f2ca06029c5ac2ebf69cb1075a993ad1894
Kj Aynera <115272232+kjayn@users.noreply.github.com>
2023-05-10 21:51:11 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,26 +1,22 @@
 import cv2
 import torch
 import numpy as np
-import telebot
-import datetime
-import threading
 from playsound import playsound
-
+import pygame
 
 model = torch.hub.load(r"C:\Users\Bob\Downloads\yolov5\content\yolov5", 'custom', path=r"C:\Users\Bob\Downloads\yolov5\content\yolov5\runs\train\results_3\weights\best.pt", source='local')
 model.conf = 0.6
 model.classes = 0
 
-# Create a Telegram bot instance with the API token
-bot = telebot.TeleBot('5935952245:AAHAaJ1ri9N5BJsq8nE7Pbw-TTYchiVieWA')
-
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture('fire2.mp4')
+        self.video = cv2.VideoCapture(0)
         self.play_sound = False
-        
+        pygame.mixer.init() # initialize pygame.mixer
+        self.sound = pygame.mixer.Sound('alarm.mp3') # load the sound file
+
     def play_sound_async(self):
-        playsound('alarm.mp3')
+        self.sound.play() # play the sound
 
     def get_frame(self):
         success, image = self.video.read()
@@ -33,29 +29,18 @@ class VideoCamera(object):
             labels = results.pandas().xyxy[0]['name']
             if 'fire' in labels.values:
                 print('Fire detected!')
-                
-                labels, cord_thres = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
-                x = cord_thres[:, 0]
-                y = cord_thres[:, 1]
-                w = cord_thres[:, 2]
-                h = cord_thres[:, 3]
-                print(x,y,w,h)
-                
+
                 if not self.play_sound:
                     # Play the sound asynchronously
                     self.play_sound = True
-                    threading.Thread(target=self.play_sound_async).start()
-                
-                # Save the image
-                cv2.imwrite('fire_detected.jpg', cv2.cvtColor(a, cv2.COLOR_RGB2BGR))
-                now = datetime.datetime.now()
-                message = f'Fire detected at {now.strftime("%Y-%m-%d %H:%M:%S")}'
-                # Send the image via Telegram
-                #with open('fire_detected.jpg', 'rb') as f:
-                    #bot.send_photo(chat_id='1937435385', photo=f, caption=message)
+                    self.play_sound_async()
+            
                 
         else:
-            self.play_sound = False
+            if self.play_sound:
+                # Stop the sound if it's playing
+                self.sound.stop()
+                self.play_sound = False
             print('no fire detected')
             
         cv2.waitKey(fps)
